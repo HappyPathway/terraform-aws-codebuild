8f3d5654f83d07e33a4d86b88ad0b293b440c21c
Dave Arnold <dave@roknsound.com>
2025-04-22 11:23:08 -0700

    Enhance outputs.tf by refining descriptions and adding badge_url and service_role outputs for CodeBuild projects

--- outputs.tf
+++ outputs.tf
@@ -5,17 +5,27 @@
 #Amazon Web Services, Inc. or Amazon Web Services EMEA SARL or both.
 
 output "id" {
-  value       = aws_codebuild_project.terraform_codebuild_project.id
-  description = "List of IDs of the CodeBuild projects"
+  description = "The name (if imported via name) or ARN (if created via Terraform or imported via ARN) of the CodeBuild project"
+  value       = { for k, v in aws_codebuild_project.terraform_codebuild_project : k => v.id }
 }
 
 output "name" {
-  value       = aws_codebuild_project.terraform_codebuild_project.name
-  description = "List of Names of the CodeBuild projects"
+  description = "The name of the CodeBuild project"
+  value       = { for k, v in aws_codebuild_project.terraform_codebuild_project : k => v.name }
 }
 
 output "arn" {
-  value       = aws_codebuild_project.terraform_codebuild_project.arn
-  description = "List of ARNs of the CodeBuild projects"
+  description = "The ARN of the CodeBuild project"
+  value       = { for k, v in aws_codebuild_project.terraform_codebuild_project : k => v.arn }
+}
+
+output "badge_url" {
+  description = "The URL of the build badge when badge_enabled is enabled"
+  value       = { for k, v in aws_codebuild_project.terraform_codebuild_project : k => v.badge_url }
+}
+
+output "service_role" {
+  description = "The ARN of the IAM service role applied to this project"
+  value       = aws_iam_role.terraform_codebuild_role.arn
 }
 
